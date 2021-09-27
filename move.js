80d5ed135a25456cdad41b2fbcc08083c576ab20
m-miyamoto <pr.h110816@gmail.com>
2021-09-27 16:59:16 +0900

    ナビの修正・スライドの動き

--- move.js
+++ move.js
@@ -2,11 +2,22 @@ $(function(){
     $('.l-hamburgerbtn--close').click(function(){
         $(this).toggleClass('l-hamburgerbtn--open');/*ボタンが×に代わる*/
         $('#slide').slideToggle(230);
+        $('.l-header__nav').toggleClass('open');
     });
 });
 
 $(function(){
-    $('.l-hamburgerbtn--close').click(function(){
-        $('.l-header__nav').toggleClass('l-header__nav--open');/*ダウンメニューの表示*/
+    $('#return a[href]').on('click', function(event) {　/*ナビをクリックするとボタンとメニューが閉じる*/ 
+        $('.l-hamburgerbtn--close').trigger('click');
     });
 });
+
+$(function() {
+    $.scrollify({
+      section : "section",
+      scrollbars:"false",//スクロールバー表示・非表示設定
+      interstitialSection : "header,footer",//ヘッダーフッターを認識し、1ページスクロールさせず表示されるように設定
+      easing: "swing", // 他にもlinearやeaseOutExpoといったjQueryのeasing指定可能
+      scrollSpeed: 300, // スクロール時の速度
+    });
+  });
